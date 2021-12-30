38af907b4f9093f5f4d1f97348fae6818053da5a
Sean Dodson <dodsonsean@comcast.net>
2021-12-30 15:05:20 -0500

    MEM-13: Add YouTube OAuth flow for user access token

--- apps/memspace-web/src/pages/_app.tsx
+++ apps/memspace-web/src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { GApiProvider } from 'react-gapi-auth2';
+
+const googleClientConfig = {
+  client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  scope: 'https://www.googleapis.com/auth/youtube.readonly',
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <GApiProvider clientConfig={googleClientConfig}>
+      <Component {...pageProps} />
+    </GApiProvider>
+  );
 }
 
 export default MyApp;
--- apps/memspace-web/src/pages/index.tsx
+++ apps/memspace-web/src/pages/index.tsx
@@ -1,7 +1,35 @@
 import type { NextPage } from 'next';
+import { useGoogleAuth, useGoogleUser } from 'react-gapi-auth2';
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
+  const loggedInUserValue = useGoogleUser();
+  const auth = useGoogleAuth();
+
+  const handleLoginClick = async () => {
+    const user = await auth.googleAuth?.signIn();
+    const authResponse = user?.getAuthResponse();
+
+    const accessToken = authResponse?.access_token;
+    console.log(accessToken);
+  };
+
+  const isLoggedIn = loggedInUserValue.currentUser !== null;
+
+  return (
+    <div>
+      <div>
+        {isLoggedIn && (
+          <>You are now logged in! Thank you for being a member.</>
+        )}
+        {!isLoggedIn && (
+          <div>
+            <div>You are not logged in.</div>
+            <button onClick={handleLoginClick}>Login</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
