fc2c4b4dd6b703d4f3cf10cf4fe4446adeef68c5
Sean Dodson <dodsonsean@comcast.net>
2021-12-29 23:22:46 -0500

    MEM-2: Setup E2E testing

---
+++ packages/youtube-member-verifier/e2e/index.test.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import { YouTubeMembersQuery } from '../src';
+
+dotenv.config({ path: './e2e/.env.e2e' });
+
+describe('YouTubeMembersQuery', () => {
+  let query: YouTubeMembersQuery;
+
+  beforeEach(() => {
+    query = new YouTubeMembersQuery({
+      apiKey: process.env.API_KEY ?? '',
+      channelId: process.env.CHANNEL_ID ?? '',
+      onBehalfOfUser: process.env.USER_BEHALF_ID ?? '',
+      authorizationHeader: process.env.AUTHORIZATION_HEADER ?? '',
+      cookieHeader: process.env.COOKIE_HEADER ?? '',
+    });
+  });
+
+  it('should return YouTube members', async () => {
+    const youTubeMembers = await query.execute();
+
+    expect(youTubeMembers.length).toBeGreaterThan(0);
+  });
+});
