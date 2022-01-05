e58c92f7eef0efa9c7dda188f61e312270ab9315
Sean Dodson <dodsonsean@comcast.net>
2022-01-04 23:16:07 -0500

    MEM-36: Add logging to login route

--- apps/authentication-server/src/routes/login.ts
+++ apps/authentication-server/src/routes/login.ts
@@ -26,31 +26,44 @@ const accessTokenGenerator = new AccessTokenGenerator(
   accessTokenConfig.expires_in
 );
 
+const logger = functions.logger;
+
 export const createLoginRouter = () => {
   const router = createRouter();
 
   router.post('/', async (req, res) => {
+    logger.info('Received login request.');
+
     const userAccessToken = req.body.accessToken;
 
     if (!userAccessToken) {
+      logger.warn('User did not provide an access token.');
       return res.sendStatus(401);
     }
 
     const youTubeChannel = await youTubeChannelQuery.execute(userAccessToken);
 
     if (!youTubeChannel) {
+      logger.warn("Unable to find user's YouTube channel.");
       return res.sendStatus(401);
     }
 
     const { id: channelId } = youTubeChannel;
 
+    logger.info("Found user's YouTube channel.", { channelId });
+
     const isMember = await isYouTubeMemberQuery.execute(channelId);
 
     if (!isMember) {
+      logger.info('User is not a channel member.', { channelId });
       return res.sendStatus(403);
     }
 
+    logger.info('Verified user is a channel member.', { channelId });
+
     const accessToken = accessTokenGenerator.generate(channelId);
+
+    logger.info('Signed access token for user.', { channelId });
 
     return res.send(accessToken);
   });
