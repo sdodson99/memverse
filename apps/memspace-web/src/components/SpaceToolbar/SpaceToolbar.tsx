4dabd5a6179213863cc687a0ec1dbe3182d31be2
Sean Dodson <dodsonsean@comcast.net>
2022-02-07 22:17:57 -0500

    MEM-47: Style space toolbar

--- apps/memspace-web/src/components/SpaceToolbar/SpaceToolbar.tsx
+++ apps/memspace-web/src/components/SpaceToolbar/SpaceToolbar.tsx
@@ -3,10 +3,18 @@ import styles from './SpaceToolbar.module.css';
 
 type SpaceToolbarProps = {};
 
-const SpaceToolbar = ({}: SpaceToolbarProps) => (
-  <div className={styles.spaceToolbar} data-testid="SpaceToolbar">
-    SpaceToolbar Component
-  </div>
-);
+const SpaceToolbar = ({}: SpaceToolbarProps) => {
+  return (
+    <div className={styles.spaceToolbar} data-testid="SpaceToolbar">
+      <div className="container">
+        <div className={styles.spaceToolbarContent}>
+          <button className={styles.toolbarItem} onClick={() => {}}>
+            🔎
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SpaceToolbar;
