eaf7b2aecff003f304c5aed2aee9f6ebc2ed0256
Sean Dodson <dodsonsean@comcast.net>
2022-02-16 22:45:52 -0500

    MEM-68: Setup Firebase analytics

--- apps/memspace-web/src/pages/_app.tsx
+++ apps/memspace-web/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import 'react-spring-bottom-sheet/dist/style.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { GApiProvider } from 'react-gapi-auth2';
 import { AccessTokenProvider } from '../hooks/authentication/use-access-token-context';
 import Head from 'next/head';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 const googleClientConfig = {
   client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
@@ -12,7 +14,23 @@ const googleClientConfig = {
   scope: 'https://www.googleapis.com/auth/youtube.readonly',
 };
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxXUSxSLuzKFuEhACAGtuvYZC-nTf70l0',
+  authDomain: 'memverse.firebaseapp.com',
+  databaseURL: 'https://memverse-default-rtdb.firebaseio.com',
+  projectId: 'memverse',
+  storageBucket: 'memverse.appspot.com',
+  messagingSenderId: '645429020198',
+  appId: '1:645429020198:web:56cdb892fda7c4c8e088e7',
+  measurementId: 'G-R4HBRGPMS3',
+};
+
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    getAnalytics();
+  }, []);
+
   return (
     <GApiProvider clientConfig={googleClientConfig}>
       <AccessTokenProvider>
