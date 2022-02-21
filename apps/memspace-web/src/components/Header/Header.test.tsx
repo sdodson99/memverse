490a326f941505e460289d6feedd74f22cf5e987
Sean Dodson <dodsonsean@comcast.net>
2022-02-21 11:58:08 -0500

    MEM-57: Add authentication header links

--- apps/memspace-web/src/components/Header/Header.test.tsx
+++ apps/memspace-web/src/components/Header/Header.test.tsx
@@ -2,13 +2,41 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Header from './Header';
+import { useIsLoggedIn } from '../../hooks/authentication/use-is-logged-in';
+
+jest.mock('../../hooks/authentication/use-is-logged-in');
+const mockUseIsLoggedIn = useIsLoggedIn as jest.Mock;
+
+jest.mock('../../hooks/authentication/use-logout');
 
 describe('<Header />', () => {
+  afterEach(() => {
+    mockUseIsLoggedIn.mockReset();
+  });
+
   it('should mount', () => {
     render(<Header />);
 
     const header = screen.getByTestId('Header');
 
     expect(header).toBeInTheDocument();
+  });
+
+  it('should render login button when not logged in', () => {
+    mockUseIsLoggedIn.mockReturnValue(false);
+    render(<Header />);
+
+    const loginButton = screen.getByText('Login');
+
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it('should render logout button when logged in', () => {
+    mockUseIsLoggedIn.mockReturnValue(true);
+    render(<Header />);
+
+    const logoutButton = screen.getByText('Logout');
+
+    expect(logoutButton).toBeInTheDocument();
   });
 });
--- apps/memspace-web/src/components/Header/Header.tsx
+++ apps/memspace-web/src/components/Header/Header.tsx
@@ -1,13 +1,45 @@
-/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
 import React from 'react';
+import { useIsLoggedIn } from '../../hooks/authentication/use-is-logged-in';
+import { useLogout } from '../../hooks/authentication/use-logout';
 import styles from './Header.module.css';
 
 type HeaderProps = {};
 
-const Header = ({}: HeaderProps) => (
-  <div className={styles.header} data-testid="Header">
-    <img className={styles.logo} src="/logo.svg" alt="Memspace Logo" />
-  </div>
-);
+const Header = ({}: HeaderProps) => {
+  const isLoggedIn = useIsLoggedIn();
+
+  const logout = useLogout();
+
+  return (
+    <div className={styles.header} data-testid="Header">
+      <div className="container">
+        <div className={styles.content}>
+          <Link href="/" passHref>
+            <a>
+              <img
+                className={styles.logo}
+                src="/logo.svg"
+                alt="Memspace Logo"
+              />
+            </a>
+          </Link>
+
+          {!isLoggedIn && (
+            <Link href="/login">
+              <a className={styles.loginButton}>Login</a>
+            </Link>
+          )}
+
+          {isLoggedIn && (
+            <button className={styles.navItem} onClick={logout}>
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
