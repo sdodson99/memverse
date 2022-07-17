9d3e1091ce751930bcd812189b4f894000d1009e
Sean Dodson <dodsonsean@comcast.net>
2022-07-17 16:38:19 -0400

    MEM-135: Connect to Firebase Admin authentication

--- apps/authentication-server/src/index.ts
+++ apps/authentication-server/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { createLoginRouter } from './routes/login';
 import { createServiceProvider } from './service-provider';
+
+admin.initializeApp();
 
 const app = express();
 app.use(cors());
