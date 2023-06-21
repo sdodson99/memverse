e3d4b66f8a8d4abdf2198f90f6e99f1f0f26d175
sdodson <sc.dodson4@gmail.com>
2023-06-21 18:32:04 -0400

    Setup linting and formatting

--- apps/showcase/src/app/layout.tsx
+++ apps/showcase/src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: 'Showcase - SingletonSean Members',
   description: 'A showcase for members of the SingletonSean YouTube channel.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
