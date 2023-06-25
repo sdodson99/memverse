c708535e3b504451f9bfe2f750e8f447187d733e
sdodson <sc.dodson4@gmail.com>
2023-06-25 18:04:42 -0400

    Remove window usage from server side

--- apps/showcase/src/features/view-members/members-stage.tsx
+++ apps/showcase/src/features/view-members/members-stage.tsx
@@ -3,15 +3,18 @@
 import { Stage } from '@pixi/react';
 import { useMembersContext } from './members-context';
 import { MemberContainer } from './member-container';
+import { useState } from 'react';
 
 export function MembersStage() {
   const { members } = useMembersContext();
 
+  const [window] = useState<Window>(() => global.window);
+
   return (
     <Stage
       options={{ backgroundColor: '#1F232D', resizeTo: window }}
-      height={window.innerHeight}
-      width={window.innerWidth}
+      height={window?.innerHeight ?? 0}
+      width={window?.innerWidth ?? 0}
     >
       {members.map((member) => (
         <MemberContainer key={member.channelId} member={member} />
