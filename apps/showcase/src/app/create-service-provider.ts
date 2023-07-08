4cb2364573384ad3c81a6f4fcb823c86103f34cd
sdodson <sc.dodson4@gmail.com>
2023-07-08 01:27:27 -0400

    Fix Firebase private key loading

--- apps/showcase/src/app/create-service-provider.ts
+++ apps/showcase/src/app/create-service-provider.ts
@@ -81,7 +81,7 @@ function createFirebaseAdminApp({ mock }: ServiceProviderOptions) {
   return initializeFirebaseAdminApp({
     credential: firebase.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
