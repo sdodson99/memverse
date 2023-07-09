875f53f0f733cff013abedc76d71abfb13ff32a1
sdodson <sc.dodson4@gmail.com>
2023-07-09 16:17:39 -0400

    Add Google Tag Manager

--- apps/showcase/src/app/layout.tsx
+++ apps/showcase/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { AuthProvider } from '@/features/auth';
 import './globals.css';
 import 'react-spring-bottom-sheet/dist/style.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Showcase - SingletonSean Members',
@@ -14,8 +15,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K4DGPB4');`}
+        </Script>
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K4DGPB4" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
