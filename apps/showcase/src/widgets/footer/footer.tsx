e669d3d1f4ccdbf605273437169690e4bbadbb31
sdodson <sc.dodson4@gmail.com>
2023-08-01 22:57:40 -0400

    Add support for links in footer

--- apps/showcase/src/widgets/footer/footer.tsx
+++ apps/showcase/src/widgets/footer/footer.tsx
@@ -1,7 +1,30 @@
+import Image from 'next/image';
+import { BsYoutube } from 'react-icons/bs';
+
+const YOUTUBE_CHANNEL_LINK = 'https://www.youtube.com/@SingletonSean';
+
 export function Footer() {
   return (
-    <footer className="bg-black text-white p-4 text-center">
-      Copyright © {new Date().getFullYear()} Sean Dodson. All rights reserved.
+    <footer className="text-white">
+      <section className="bg-zinc-900">
+        <div className="content-container flex flex-col items-center p-8 sm:flex-row sm:justify-between sm:items-start mx-auto">
+          <Image src="/logo-light.png" alt="" width="75" height="75" />
+          <div className="mt-8 sm:mt-0 sm:ml-8">
+            <a
+              className="flex items-center hover:underline"
+              href={YOUTUBE_CHANNEL_LINK}
+              target="_blank"
+              referrerPolicy="no-referrer"
+            >
+              <BsYoutube />
+              <span className="ml-2">Channel</span>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="p-4 text-center bg-black text-sm">
+        Copyright © {new Date().getFullYear()} Sean Dodson. All rights reserved.
+      </section>
     </footer>
   );
 }
